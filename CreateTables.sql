a5f7e81b733b23835404cf10e064a5b85e6223f0
kopgithub <57072372+kopgithub@users.noreply.github.com>
2019-10-28 23:07:19 +0300

    Add files via upload

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,4 +1,4 @@
--- Сначала удалим имеющиеся версии таблиц (начинаем с таблицы связей что бы не мешали ключи)
+-- РЎРЅР°С‡Р°Р»Р° СѓРґР°Р»РёРј РёРјРµСЋС‰РёРµСЃСЏ РІРµСЂСЃРёРё С‚Р°Р±Р»РёС† (РЅР°С‡РёРЅР°РµРј СЃ С‚Р°Р±Р»РёС†С‹ СЃРІСЏР·РµР№ С‡С‚Рѕ Р±С‹ РЅРµ РјРµС€Р°Р»Рё РєР»СЋС‡Рё)
 IF OBJECT_ID (N'CrossArtTag', N'U') IS NOT NULL
 	DROP TABLE CrossArtTag;
 IF OBJECT_ID (N'Article', N'U') IS NOT NULL
@@ -7,7 +7,7 @@ IF OBJECT_ID (N'Tag', N'U') IS NOT NULL
 	DROP TABLE Tag;
 
 -----------------------------------------------------------------------------------
--- Создаем таблицу статей
+-- РЎРѕР·РґР°РµРј С‚Р°Р±Р»РёС†Сѓ СЃС‚Р°С‚РµР№
 CREATE TABLE Article(
 	id int NOT NULL,
 	name nvarchar(50) NOT NULL,
@@ -15,14 +15,14 @@ CREATE TABLE Article(
 	WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
--- Создаем уникальный индекс по имени статей (чтобы не заводили статьи с одинаковыми именами)
+-- РЎРѕР·РґР°РµРј СѓРЅРёРєР°Р»СЊРЅС‹Р№ РёРЅРґРµРєСЃ РїРѕ РёРјРµРЅРё СЃС‚Р°С‚РµР№ (С‡С‚РѕР±С‹ РЅРµ Р·Р°РІРѕРґРёР»Рё СЃС‚Р°С‚СЊРё СЃ РѕРґРёРЅР°РєРѕРІС‹РјРё РёРјРµРЅР°РјРё)
 CREATE UNIQUE NONCLUSTERED INDEX IX_Article_Name ON Article(
 	name ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 
 -----------------------------------------------------------------------------------
--- Создаем таблицу Тэгов
+-- РЎРѕР·РґР°РµРј С‚Р°Р±Р»РёС†Сѓ РўСЌРіРѕРІ
 CREATE TABLE Tag(
 	id int NOT NULL,
 	name nvarchar(50) NOT NULL,
@@ -30,14 +30,14 @@ CREATE TABLE Tag(
 	WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
--- Создаем уникальный индекс по имени тэга (чтобы не заводили тэги с одинаковыми именами)
+-- РЎРѕР·РґР°РµРј СѓРЅРёРєР°Р»СЊРЅС‹Р№ РёРЅРґРµРєСЃ РїРѕ РёРјРµРЅРё С‚СЌРіР° (С‡С‚РѕР±С‹ РЅРµ Р·Р°РІРѕРґРёР»Рё С‚СЌРіРё СЃ РѕРґРёРЅР°РєРѕРІС‹РјРё РёРјРµРЅР°РјРё)
 CREATE UNIQUE NONCLUSTERED INDEX IX_Tag_Name ON Tag(
 	name ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 
 -----------------------------------------------------------------------------------
--- Создаем таблицу перекрестных связей (многие-ко-многим) Статьи-Тэги
+-- РЎРѕР·РґР°РµРј С‚Р°Р±Р»РёС†Сѓ РїРµСЂРµРєСЂРµСЃС‚РЅС‹С… СЃРІСЏР·РµР№ (РјРЅРѕРіРёРµ-РєРѕ-РјРЅРѕРіРёРј) РЎС‚Р°С‚СЊРё-РўСЌРіРё
 CREATE TABLE CrossArtTag(
 	ArtId int NOT NULL,
 	TagId int NOT NULL,
@@ -45,18 +45,18 @@ CREATE TABLE CrossArtTag(
 	WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
--- Создаем внешний ключ для статей
+-- РЎРѕР·РґР°РµРј РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ СЃС‚Р°С‚РµР№
 ALTER TABLE CrossArtTag WITH CHECK ADD CONSTRAINT FK_CrossArtTag_ArtId FOREIGN KEY(ArtId)
 REFERENCES Article(id)
 GO
--- Создаем внешний ключ для тэгов
+-- РЎРѕР·РґР°РµРј РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚СЌРіРѕРІ
 ALTER TABLE CrossArtTag WITH CHECK ADD CONSTRAINT FK_CrossArtTag_TagId FOREIGN KEY(TagId)
 REFERENCES Tag(id)
 GO
 
 -----------------------------------------------------------------------------------
--- Предположим у нас много однотипных таблиц и их надо заполнить 
--- Делаем универсальную процедуру для заполнения таких таблиц чтобы избежать копипасты
+-- РџСЂРµРґРїРѕР»РѕР¶РёРј Сѓ РЅР°СЃ РјРЅРѕРіРѕ РѕРґРЅРѕС‚РёРїРЅС‹С… С‚Р°Р±Р»РёС† Рё РёС… РЅР°РґРѕ Р·Р°РїРѕР»РЅРёС‚СЊ 
+-- Р”РµР»Р°РµРј СѓРЅРёРІРµСЂСЃР°Р»СЊРЅСѓСЋ РїСЂРѕС†РµРґСѓСЂСѓ РґР»СЏ Р·Р°РїРѕР»РЅРµРЅРёСЏ С‚Р°РєРёС… С‚Р°Р±Р»РёС† С‡С‚РѕР±С‹ РёР·Р±РµР¶Р°С‚СЊ РєРѕРїРёРїР°СЃС‚С‹
 drop PROCEDURE FillTable
 GO
 CREATE PROCEDURE FillTable(@count int, @table nvarchar(30), @prefix nvarchar(30)) AS
@@ -74,13 +74,13 @@ BEGIN
 END
 GO
 
--- Заполняем таблицу статей
+-- Р—Р°РїРѕР»РЅСЏРµРј С‚Р°Р±Р»РёС†Сѓ СЃС‚Р°С‚РµР№
 execute dbo.FillTable 4, N'Article', N'Art_';
 
--- Заполняем таблицу тэгов
+-- Р—Р°РїРѕР»РЅСЏРµРј С‚Р°Р±Р»РёС†Сѓ С‚СЌРіРѕРІ
 execute dbo.FillTable 5, N'Tag', N'#Tag_';
 
--- Заполняем связи Статей и Тэгов
+-- Р—Р°РїРѕР»РЅСЏРµРј СЃРІСЏР·Рё РЎС‚Р°С‚РµР№ Рё РўСЌРіРѕРІ
 INSERT INTO CrossArtTag(ArtId, TagId) VALUES(1, 1);
 INSERT INTO CrossArtTag(ArtId, TagId) VALUES(1, 2); 
 INSERT INTO CrossArtTag(ArtId, TagId) VALUES(1, 3);
@@ -89,5 +89,5 @@ INSERT INTO CrossArtTag(ArtId, TagId) VALUES(2, 3);
 INSERT INTO CrossArtTag(ArtId, TagId) VALUES(3, 3);
 INSERT INTO CrossArtTag(ArtId, TagId) VALUES(3, 4);
 
--- Все создано и
--- Теперь можно выполнять запросы из фала Task3.sql
+-- Р’СЃРµ СЃРѕР·РґР°РЅРѕ Рё
+-- РўРµРїРµСЂСЊ РјРѕР¶РЅРѕ РІС‹РїРѕР»РЅСЏС‚СЊ Р·Р°РїСЂРѕСЃС‹ РёР· С„Р°Р»Р° Task3.sql
